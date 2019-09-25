aa7f06c15789db721043b828d5728120f77d19e0
DanielChisica <danielchisica777@gmail.com>
2019-09-25 13:34:19 -0500

    Login

--- backend/conn/db.js
+++ backend/conn/db.js
@@ -1,5 +1,5 @@
 //
 
 module.exports = {
-    DB:'mongodb://localhost:27017/agencia' //conexion a la una base de datos
+    DB:'mongodb+srv://daniel:mern1234@cluster0-nx2vb.gcp.mongodb.net/test?retryWrites=true&w=majority'//conexion a la una base de datos
 }
