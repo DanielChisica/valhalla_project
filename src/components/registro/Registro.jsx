c65369e314e51adb42f38a6157811f18f634773f
DanielChisica <danielchisica777@gmail.com>
2019-10-16 04:40:20 -0500

    Campos requeridos en login

--- src/components/registro/Registro.jsx
+++ src/components/registro/Registro.jsx
@@ -7,9 +7,9 @@ class Registro extends Component {
     render() {
         return (
             <div className="Registro">
-                <TextBox tipo="text" titulo="Nombre" />
-                <TextBox tipo="text" titulo="Apellido" />
-                <TextBox tipo="text" titulo="Numero" />
+                <TextBox tipo="text" titulo="Nombre"/>
+                <TextBox tipo="text" titulo="Apellido"/>
+                <TextBox tipo="text" titulo="Numero"/>
                 <TextBox tipo="text" titulo="Cedula" />
                 <TextBox tipo="text" titulo="Correo" />
                 <TextBox tipo="password" titulo="Contraseña" />
